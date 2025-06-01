9d62b18666ca85e3c191fcc94386dc8fac08e671
kumarmanish562 <qmanishkumar562@gmail.com>
2025-06-01 21:37:17 +0530

    Adds OurBestSellers component to the Home page
    Introduces a new component to showcase top-rated books, enhancing the user experience with smooth scrolling navigation.
    
    Integrates the new component into the Home page layout to present curated selections effectively.

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from  "../../components/Navbar/Navbar"
 import Banner from '../../components/Banner/Banner'
+import OurBestSellers from '../../components/OurBestSellers/OurBestSellers'
 
 const Home = () => {
   return (
     <>
     <Navbar />
     <Banner />
+    <OurBestSellers />
     </>
   )
 }
